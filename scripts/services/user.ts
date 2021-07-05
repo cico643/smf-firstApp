c8b2c388209351fbcfffb87b1fb79ede70f6f285
cihat.yesildag@smartface.io <cihat.yesildag@smartface.io>
2021-07-05 10:19:21 +0000

    fixed unnecessary promise return function

--- scripts/services/user.ts
+++ scripts/services/user.ts
@@ -1,24 +1,22 @@
 import { sc } from "./serviceConfig";
-export function login( username: string, password: string)  {
-    return new Promise( async ( resolve, reject ) => {
+export async function login( username: string, password: string)  {
             try {
-            const response: string = await sc.request(`/auth-test/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: {
-                name: username,
-                password
-            }
-            })
+                const response: string = await sc.request(`/auth-test/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: {
+                    name: username,
+                    password
+                }
+                });
 
-            resolve(response);
+                return response;
             } catch (err) {
                 console.error(err);
-                reject(err);
+                throw err;
             }
         
-       })     
 }
 
